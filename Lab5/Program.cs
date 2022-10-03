df565884fa1e331721d63f8a2623ac2ad32da251
JLW157 <90110681+JLW157@users.noreply.github.com>
2022-10-03 15:12:03 +0300

    Add project files.

---
+++ Lab5/Program.cs
@@ -0,0 +1,72 @@
+﻿int[] array = { 73, 57, 49, 99, 42 };
+
+array = MergeSort(array, 0, array.Length - 1);
+
+ShowArray(array);
+
+int[] MergeSort(int[] array, int lowIndex, int highIndex)
+{
+    if (lowIndex < highIndex)
+    {
+        var middleIndex = (lowIndex + highIndex) / 2;
+
+        MergeSort(array, lowIndex, middleIndex);
+        MergeSort(array, middleIndex + 1, highIndex);
+
+        Merge(array, lowIndex, middleIndex, highIndex);
+    }
+
+    return array;
+}
+
+void Merge(int[] array, int lowIndex, int middleIndex, int highIndex)
+{
+    var left = lowIndex;
+    var right = middleIndex + 1;
+    var tempArray = new int[highIndex - lowIndex + 1];
+    var index = 0;
+
+    while ((left <= middleIndex) && (right <= highIndex))
+    {
+        if (array[left] < array[right])
+        {
+            tempArray[index] = array[left];
+            left++;
+        }
+        else
+        {
+            tempArray[index] = array[right];
+            right++;
+        }
+
+        index++;
+    }
+
+    for (var i = left; i <= middleIndex; i++)
+    {
+        tempArray[index] = array[i];
+        index++;
+    }
+
+    for (var i = right; i <= highIndex; i++)
+    {
+        tempArray[index] = array[i];
+        index++;
+    }
+
+    // Переписуємо всі значення з тимчасового масиву в основний
+    for (var i = 0; i < tempArray.Length; i++)
+    {
+        array[lowIndex + i] = tempArray[i];
+    }
+}
+
+// merge sort
+
+void ShowArray(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.WriteLine(item);
+    }
+}
